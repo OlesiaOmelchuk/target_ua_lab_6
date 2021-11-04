21db4dcc9b1db766050a5c41be54945760e9f9a9
Olesia <olesyao2003@gmail.com>
2021-11-04 19:12:26 +0200

    finished_0,21

--- target_ua.py
+++ target_ua.py
@@ -1,8 +1,9 @@
 """Target game Ukrainian version"""
 import random
+from typing import List
 
 
-def generate_grid():
+def generate_grid() -> List[str]:
     """
     Generates list of letters
     """
@@ -15,7 +16,7 @@ def generate_grid():
     return grid_list
 
 
-def get_words(path, letters):
+def get_words(path: str, letters: List[str]) -> List[str]:
     """
     Reads the file path. Checks the words with rules and returns a list of \
 tuples: word and part of speech
@@ -35,15 +36,24 @@ tuples: word and part of speech
                     words_list.append((parts[0], 'adjective'))
                 if 'adv' in parts[1]:
                     words_list.append((parts[0], 'adverb'))
-    print(words_list)
+    return words_list
 
 
-print(get_words('base.lst', ['щ']))
-
-
-def check_user_words(user_words, language_part, letters, dict_of_words):
+def check_user_words(user_words: List[str], language_part: str, letters: List[str],
+                     dict_of_words: List[str]) -> List[List[str]]:
     """
     Checks user's words. Returns list of right user's words and list of words\
 which user didn't mention.
     """
-    pass
+    right_user_words = []
+    for word in user_words:
+        if 1 <= len(word) <= 5:
+            if word[0] in letters:
+                dict_length = len(dict_of_words)
+                for i in range(dict_length):
+                    if word == dict_of_words[i][0] and dict_of_words[i][1] == language_part:
+                        right_user_words.append(word)
+
+    other_words = [word[0] for word in dict_of_words if word[0]
+                   not in right_user_words and word[1] == language_part]
+    return right_user_words, other_words
